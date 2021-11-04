215675c36c845a65c03e91137bc4fa934bbaba38
Sandeep Kumar <sandeep.1310128@kiet.edu>
2021-11-04 11:45:26 +0530

    added today

--- src/components/_dashboard/app/AppItemOrders.js
+++ src/components/_dashboard/app/AppItemOrders.js
@@ -36,15 +36,15 @@ const IconWrapperStyle = styled('div')(({ theme }) => ({
 
 const TOTAL = 1723315;
 
-export default function AppItemOrders() {
+export default function AppItemOrders(props) {
   return (
     <RootStyle>
       <IconWrapperStyle>
         <Icon icon={windowsFilled} width={24} height={24} />
       </IconWrapperStyle>
-      <Typography variant="h3">{fShortenNumber(TOTAL)}</Typography>
+      <Typography variant="h3">{fShortenNumber(props.fit)}</Typography>
       <Typography variant="subtitle2" sx={{ opacity: 0.72 }}>
-        Today Fitness
+        Weekly Expiry Fitness
       </Typography>
     </RootStyle>
   );
--- src/pages/DashboardApp.js
+++ src/pages/DashboardApp.js
@@ -3,6 +3,8 @@ import { Grid, Container, Typography, Button, Stack } from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom';
 import plusFill from '@iconify/icons-eva/plus-fill';
 import { Icon } from '@iconify/react';
+import { useState, useEffect } from 'react';
+
 
 // components
 import Page from '../components/Page';
@@ -21,15 +23,42 @@ import {
   AppConversionRates
 } from '../components/_dashboard/app';
 
+const token = localStorage.getItem('token');
+
+
 // ----------------------------------------------------------------------
 
 export default function DashboardApp(props) {
-  console.log(props.token);
+
+  const [loader, setLoader] = useState(true);
+  const [info, setInfo] = useState({});
+
+  useEffect(() => {
+    const myHeaders = new Headers({
+      'Content-Type': 'application/json',
+      'x-access-token': token
+    });
+    const requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+    };
+    fetch(process.env.REACT_APP_BACKEND_API + '/info', requestOptions)
+      .then(res => res.json())
+      .then((result) => {
+        setInfo(result.info);
+        setLoader(false);
+      })
+      .catch((e) => {
+        setInfo({});
+      })
+  }, [])
+
+
   return (
     <Page title="Dashboard | Minimal-UI">
       <Container maxWidth="xl">
         <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-        <Typography variant="h4">Hi, Welcome back</Typography>
+          <Typography variant="h4">Hi, Welcome back</Typography>
           <Button
             variant="contained"
             component={RouterLink}
@@ -41,16 +70,16 @@ export default function DashboardApp(props) {
         </Stack>
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={3}>
-            <AppWeeklySales />
+            <AppWeeklySales puc = {info.puc || 0}/>
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
-            <AppNewUsers />
+            <AppNewUsers ins = {info.ins || 0}/>
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
-            <AppItemOrders />
+            <AppItemOrders fit = {info.fit || 0} />
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
-            <AppBugReports />
+            <AppBugReports today = {info.today} total = {info.total}/>
           </Grid>
 
           <Grid item xs={12} md={6} lg={8}>
@@ -70,7 +99,7 @@ export default function DashboardApp(props) {
           </Grid>
 
           <Grid item xs={12} md={6} lg={8}>
-          <AppTasks />
+            <AppTasks />
           </Grid>
 
           <Grid item xs={12} md={6} lg={4}>
